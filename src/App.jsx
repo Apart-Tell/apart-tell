b6985a0cccac3dd9dda315ea861977bc02cd6298
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2023-04-25 21:14:33 +0800

    Firebase keys
    added firebase keys (functionality to be checked)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvm43S5VJsmg8pF_tLfhvCIFYSnXM85Bg",
+  authDomain: "apart-tell.firebaseapp.com",
+  databaseURL: "https://apart-tell-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "apart-tell",
+  storageBucket: "apart-tell.appspot.com",
+  messagingSenderId: "435778951435",
+  appId: "1:435778951435:web:19a2d4a9ee6f5580112a0e",
+  measurementId: "G-21WQSCSH3V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +48,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more hi
       </p>
     </div>
   )
