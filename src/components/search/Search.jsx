0d5f3a6befd3ad61a3f38f9a6079cbd6f3e4cc55
sanyapi <slpiang@up.edu.ph>
2023-05-02 10:41:21 +0800

    added comments for the responsive search box/container

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -10,8 +10,10 @@ export default function Search(){
             <h1>Experience comfort and community,<br></br>
             tell us where you want to be.</h1>
               <div className='search-box-container'>
+                {/**enclose search bar inside div */}
                 <form action='' className='search-bar'>
                   <input type='text'></input><br></br>
+                  {/**enclose search button inside div */}
                   <button type='submit'>Search</button>
                 </form>
             </div>
