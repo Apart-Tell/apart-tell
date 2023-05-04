0a4831d95c4d05a869d8e694513cb06ce6aff373
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2023-05-05 00:21:53 +0800

    login page update
    added authdetail component to tell if current user is signed in , and to sign out
    
    added login page using register scss
    
    added added header for register jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,87 @@
-import React from 'react'
-import './login.scss';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
+import React, { useState } from "react";
+import "./login.scss";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import AuthDetails from "../../components/firebase/AuthDetails";
 
-function Login () {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-    <Header/>
-    <div>This is the Login Page!</div>
-    <Footer/>
+      <Header />
+      <div className="logo1">
+        <a>
+          <img src="logo-icon.png" alt="apart-tell logo" />
+        </a>
+      </div>
+      <div className="login-container">
+        <form onSubmit={login}>
+          <h1 className="Register-text">Log In</h1>
+
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="inputEmail"
+          ></input>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="inputPword"
+          ></input>
+          <button type="submit" className="Login-Button">
+            Log In
+          </button>
+          <div class="or-lines">
+            <hr class="linea"></hr>
+            <span class="or-textA">or</span>
+            <hr class="line-1a"></hr>
+          </div>
+
+          <button className="Google-ButtonA" type="submit">
+            Log In with Google
+          </button>
+          {/*images*/}
+          <img src="Vector.png" alt="apart-tell vector" className="vectorA" />
+          <img src="lock.png" alt="apart-tell vector1" className="lockA" />
+        </form>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <AuthDetails />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
     </>
-  
-  )
+  );
 }
 
-export default Login
+export default Login;
