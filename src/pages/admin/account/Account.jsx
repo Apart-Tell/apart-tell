adc21876580d66a325e8b585754ba8c12d92f5e2
Sanyiah <slpiang@up.edu.ph>
2023-05-10 18:42:46 +0800

    nested h2 and h4 elements under their classes - option.scss

--- src/pages/admin/account/Account.jsx
+++ src/pages/admin/account/Account.jsx
@@ -7,56 +7,12 @@ const Account = () => {
   return (
     <>
     <Header/>
-      <div className='myacc'>
-        My Account
-      </div>
-      <div className='personal'>
-        Personal Information
-      </div>
-      <div className=''>
-        <form>
-          <input type='text' className=''/>
-          <input type='text' className=''/>
-          <input type='text' className=''/>
-          <input type='text' className=''/>
-        </form>
-        <hr className='hr1'></hr>
-        <form>
-         <input type='text' className=''/>
-        </form>
-        <form>
-         <input type='text' className=''/>
-         <input type='text' className=''/>
-         <input type='text' className=''/>
-
-        </form>
-      </div>
-
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/> 
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-    <Footer className="footer"/>
+    <div className="container">
+      <h1>My Account</h1>
+      <h2>Personal Information</h2>
+    </div>
+    <Footer/>
+  
     </>
   )
 }
