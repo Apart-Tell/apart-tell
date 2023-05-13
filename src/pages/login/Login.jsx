0f8d4ae6f20ad351ab1dee74c3d34cd7848a27ae
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2023-05-13 12:49:12 +0800

    log in

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -66,7 +66,7 @@ function Login() {
       <br />
       <AuthDetails />
       <br />
-      <a href="/admin-home">Testing</a>
+
       {/* end of testing section */}
 
       <div className="footer">
