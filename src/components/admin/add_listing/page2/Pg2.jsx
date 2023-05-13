24b0ac553d3b2f0ebc34592b074adaf35de6f2ee
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2023-05-13 12:28:06 +0800

    ap-10

--- src/components/admin/add_listing/page2/Pg2.jsx
+++ src/components/admin/add_listing/page2/Pg2.jsx
@@ -1,120 +1,190 @@
-import React from 'react';
-import { useState } from 'react';
-import './pg2.scss';
-
+import React from "react";
+import { useState } from "react";
+import "./pg2.scss";
+import { useNavigate } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  serverTimestamp,
+} from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { db } from "../../../../firebase";
 const MAX_COUNT = 4;
 
 const Pg2 = () => {
+  // const navigate=useNavigate();
+  // const {docUid}=navigate.getState().state;
   // handles occupants data
-  const [occupants, setOccupants] = useState('');
-  // handles dimensions data
-  const [dimension, setDimension] = useState('');
-   // handles CR type data
-  const [CRType, setCRType] = useState('');
-  // handles file upload
-  const [uploadedFiles, setUploadedFiles] = useState([])
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [formData, setFormData] = useState({
+    occupants: "",
+    dimensions: "",
+    crType: "",
+  });
+  const [uploadedFiles, setUploadedFiles] = useState([]);
   const [fileLimit, setFileLimit] = useState(false);
 
   // handles input change for OCCUPANTS
   const handleInputChange = (e) => {
     const { id, value } = e.target;
-    if (id === 'occupants-room') {
-      setOccupants(value);
-    } else if (id === 'room-dimension') {
-      setDimension(value);
-    } else if (id === 'communal' || id === 'private') {
-      setCRType(value);
-    }
+    setFormData((prevFormdata) => {
+      return {
+        ...prevFormdata,
+        [id]: value,
+      };
+    });
+    validateForm();
+    console.log(id, value);
   };
 
   // handles file uplaod
-  const handleUploadFiles = files => {
+  const handleUploadFiles = async (files) => {
     const uploaded = [...uploadedFiles];
     let limitExceeded = false;
-      files.some((file) => {
-        if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-          uploaded.push(file);
-            if (uploaded.length === MAX_COUNT) setFileLimit(true);
-              if (uploaded.length > MAX_COUNT) {
-                alert(`You can only add a maximum of ${MAX_COUNT} files`);
-                setFileLimit(false);
-                limitExceeded = true;
-                return true;
-              }
-            }
-      })
-    if (!limitExceeded) setUploadedFiles(uploaded)
-  }
+    for (const file of files) {
+      if (uploaded.findIndex((f) => f.name === file.name) === -1) {
+        uploaded.push(file);
+        if (uploaded.length === MAX_COUNT) setFileLimit(true);
+        if (uploaded.length > MAX_COUNT) {
+          alert(`You can only add a maximum of ${MAX_COUNT} files`);
+          setFileLimit(false);
+          limitExceeded = true;
+          break;
+        } else {
+          const storageRef = ref(getStorage(), `room_photos/${file.name}`);
+          await uploadBytes(storageRef, file);
+        }
+      }
+    }
+    if (!limitExceeded) setUploadedFiles(uploaded);
+  };
 
-  const handleFileEvent =  (e) => {
-    const chosenFiles = Array.prototype.slice.call(e.target.files)
+  const handleFileEvent = (e) => {
+    const chosenFiles = Array.from(e.target.files);
     handleUploadFiles(chosenFiles);
-  }
+  };
+
+  const validateForm = () => {
+    const { occupants, dimensions, crType } = formData;
+    if (occupants == "" || dimensions == "" || crType == "") {
+      setIsFormValid(false);
+    } else {
+      setIsFormValid(true);
+    }
+  };
 
   // checks whether all input fields are filled out
-  const handleNextClick = (e) => {
+  const handleNextClick = async (e) => {
     // Perform any additional validation if needed
-    if (!occupants || !dimension || !CRType) {
-        e.preventDefault();
-        alert('Please fill in all the required fields.');
-    } else {
-      // Can proceed to next page
-      window.location.href = '/page3';
+    if (!isFormValid) {
+      alert("Please fill in all the required fields.");
+      return;
+    }
+
+    try {
+      const newDocRef = await addDoc(collection(db, "accommodations"), {
+        ...formData,
+        progress: 2,
+        // photos: photoRefs,
+        createdAt: serverTimestamp(),
+      });
+      console.log("Document written with ID: ", newDocRef.id);
+      window.location.href = "/page3";
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
   };
 
   return (
     <>
-    <div className='wrapper container'>
+      <div className="wrapper container">
         <h2>Room Details</h2>
         <form>
           <div>
-            <label htmlFor='occupants-room'>Number of occupants per room</label>
-            <input type="number" id='occupants-room' min={1} value={occupants} onChange={handleInputChange} required></input>
+            <label htmlFor="occupants-room">Number of occupants per room</label>
+            <input
+              type="number"
+              id="occupants"
+              min={1}
+              onChange={handleInputChange}
+              required
+            ></input>
           </div>
           <div>
-            <br/>
-            <label htmlFor='room-dimension'>Room Dimension</label>
-            <input type='text' id="room-dimension" value={dimension} onChange={handleInputChange} required></input>
+            <br />
+            <label htmlFor="room-dimension">Room Dimension</label>
+            <input
+              type="text"
+              id="dimensions"
+              onChange={handleInputChange}
+              required
+            ></input>
           </div>
           <div>
-            <br/>
+            <br />
             <label>Comfort Room Type</label>
             <span>
-              <input type="radio" id="communal" name='cr-type'  value='communal' checked={CRType === 'communal'} onChange={handleInputChange} required></input>
-              <label htmlFor='communal'> Communal/Shared</label>
+              <input
+                type="radio"
+                id="crType"
+                name="crType"
+                value="communal"
+                onChange={handleInputChange}
+                required
+              ></input>
+              <label htmlFor="communal"> Communal/Shared</label>
             </span>
             <span>
-              <input type='radio' id="private" name='cr-type' value='private' checked={CRType === 'private'} onChange={handleInputChange} required></input>
-              <label htmlFor='private'> Private</label>
+              <input
+                type="radio"
+                id="crType"
+                name="crType"
+                value="private"
+                onChange={handleInputChange}
+                required
+              ></input>
+              <label htmlFor="private"> Private</label>
             </span>
           </div>
-        <br></br> 
-        <div>
-          <label htmlFor="photos">Upload photos</label>
-          <br/>
-          <input id='fileUpload' type='file' multiple accept='image/png'  onChange={handleFileEvent} disabled={fileLimit} required/>
-          <br/>
+          <br></br>
+          <div>
+            <label htmlFor="photos">Upload photos</label>
+            <br />
+            <input
+              id="fileUpload"
+              type="file"
+              multiple
+              accept="image/png"
+              onChange={handleFileEvent}
+              disabled={fileLimit}
+              required
+            />
+            <br />
 
-          <label htmlFor='fileUpload'>
-            <br/>
-				    <a  className={`btn btn-primary ${!fileLimit ? '' : 'disabled' } `}>Uploaded Files</a>
-			    </label>
+            <label htmlFor="fileUpload">
+              <br />
+              <a className={`btn btn-primary ${!fileLimit ? "" : "disabled"} `}>
+                Uploaded Files
+              </a>
+            </label>
 
-          <div className="uploaded-files-list">
-            {uploadedFiles.map(file => (
-              <div >
-                {file.name}
-              </div>
-            ))}
+            <div className="uploaded-files-list">
+              {uploadedFiles.map((file) => (
+                <div>{file.name}</div>
+              ))}
+            </div>
           </div>
-        </div>
-          <button type="button" className='prev-btn'><a href='/page1'>Prev</a></button>
-          <button type="button" className='next-btn' onClick={handleNextClick}><a>Next</a></button>
+          <button type="button" className="prev-btn">
+            <a href="/page1">Prev</a>
+          </button>
+          <button type="button" className="next-btn" onClick={handleNextClick}>
+            <a>Next</a>
+          </button>
         </form>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Pg2;
--- src/components/admin/crud/CRUD.jsx
+++ src/components/admin/crud/CRUD.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import "./crud.scss";
-import { HiPlus } from 'react-icons/hi';
+import { HiPlus } from "react-icons/hi";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const CRUD = () => {
+  const getAllAccommodations = async () => {
+    const accommodationsCollectionRef = collection(db, "accommodations");
+    const accommodationsSnapshot = await getDocs(accommodationsCollectionRef);
+    const accommodations = accommodationsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return accommodations;
+  };
+
+  useEffect(() => {
+    const getAccommodations = async () => {
+      const accommodations = await getAllAccommodations();
+      console.log(accommodations);
+    };
+    getAccommodations();
+  }, []);
   return (
     <>
-    <div className='crud-wrapper container'>
-        <h2 className='directory-text'>My Directory</h2>
-        <div className='search-add'>
-            <input type='text' placeholder='Search by name'></input>
-            <button className='search-btn button'>
-                <a href='/'>Search</a>
-            </button>
-            <button className='add-btn button'>
-                <a href='/page1'> + Add New Listing</a>
-            </button>
+      <div className="crud-wrapper container">
+        <h2 className="directory-text">My Directory</h2>
+        <div className="search-add">
+          <input type="text" placeholder="Search by name"></input>
+          <button className="search-btn button">
+            <a href="/">Search</a>
+          </button>
+          <button className="add-btn button">
+            <a href="/page1"> + Add New Listing</a>
+          </button>
         </div>
-        <div className='crud-table'>
-        <table>
+        <div className="crud-table">
+          <table>
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th>Action</th>
-                </tr>
+              <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Action</th>
+              </tr>
             </thead>
-            <tbody>
-            </tbody>
-        </table>
+            <tbody></tbody>
+          </table>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CRUD
+export default CRUD;
