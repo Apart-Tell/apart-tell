3a46cabe6996f733ac1925165c51f083dee1d5dd
DanicaVont <daapostol2@up.edu.ph>
2023-05-17 01:56:03 +0800

    clean up

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import './search.scss';
 
-export default function Search(){
+export default function Search() {
   return (
     <section>
-      <div className = "container">
+      <div className="container">
         <div className='search'>
           <div className='search-text'>
-            <h1>Experience comfort and community,<br/>tell us where you want to be.</h1>
+            <h1>Experience comfort and community,<br />tell us where you want to be.</h1>
           </div>
-              <div className='search-box-container'>
-                  {/**enclose search bar inside div */}
-                  <div className='search-bar-container'>
-                    <input type='text'></input><br></br>
-                  </div>
-                  {/**enclose search button inside div */}
-                  <div className='submit-btn-container'>
-                    <button>Search</button> 
-                  </div>
+          <div className='search-box-container'>
+            {/**enclose search bar inside div */}
+            <div className='search-bar-container'>
+              <input type='text'></input><br></br>
             </div>
+            {/**enclose search button inside div */}
+            <div className='submit-btn-container'>
+              <button>Search</button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
