e542abeb18e0a99403d13fdfea984b500d42b4b8
sanyapi <slpiang@up.edu.ph>
2023-05-31 03:49:49 +0800

    improved styling and mobile responsiveness of checklist component

--- src/components/explore/checklist/Checklist.jsx
+++ src/components/explore/checklist/Checklist.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './checklist.scss';
 
 const Checklist = () => {
-    return (
-        <>
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
-        <div className="checklist-section">
-            <div className="type-selection checklist-item">
-                <label>Type</label>
-                <select>
-                    <option>Select a type</option>
-                    <option>Apartment</option>
-                    <option>Boarding House</option>
-                    <option>Apartment</option>
-                </select>
-                <hr />
-            </div>
-            <form className="rental-fee-room checklist-item">
-                    <label htmlFor="">Rental Fee (per room)</label>
-                    <input type="text" />
-                    <hr />
-            </form>
-            <form className="rental-fee-head checklist-item">
-                    <label htmlFor="">Rental Fee (per head)</label>
-                    <input type="text" />
-            </form>
+  const toggleDropdown = () => {
+    setIsDropdownVisible((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <div className="checklist-section">
+        <div className="filter-icon" onClick={toggleDropdown}>
+          <img src="src/assets/svg/filter.svg" alt="Filter Icon" />
         </div>
 
-        </>
-    )
-
-}
+        {isDropdownVisible && (
+          <div className="checklist-items">
+            <div className="type-selection checklist-item">
+              <label>Type</label>
+              <select>
+                <option>Select a type</option>
+                <option>Apartment</option>
+                <option>Boarding House</option>
+                <option>Apartment</option>
+              </select>
+              <hr />
+            </div>
+            <form className="rental-fee-room checklist-item">
+              <label htmlFor="">Rental Fee (per room)</label>
+              <input type="text" />
+              <hr />
+            </form>
+            <form className="rental-fee-head checklist-item">
+              <label htmlFor="">Rental Fee (per head)</label>
+              <input type="text" />
+            </form>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Checklist;
