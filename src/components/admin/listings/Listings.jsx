44409fe7ca59d215d2b01d2304c9cd04dca26b62
Meekah Yzabelle A. Carballo <91593459+macarballo@users.noreply.github.com>
2023-05-31 03:30:37 +0800

    AP-12-view-individual-listings
    Added the following functionalities:
    
    1. Displayed 3 out of all the listings from the Firebase database.
    2. Did some modifications to the styling of the "cards" in Home.
    
    Issue:
    Responsiveness

--- src/components/admin/listings/Listings.jsx
+++ src/components/admin/listings/Listings.jsx
@@ -1,71 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, doc, updateDoc, query, where, orderBy, limit } from 'firebase/firestore';
+import { db, auth } from '../../../firebase';
 
 const Listings = () => {
-    const handleExploreClick = () => {
-        window.location.href = '/admin-explore';
+  const [accommodations, setAccommodations] = useState([]);
+
+  useEffect(() => {
+    const fetchAccommodations = async () => {
+      try {
+        const accommodationsCollectionRef = collection(db, 'accommodations');
+        const q = query(
+          accommodationsCollectionRef,
+          orderBy('editedAt', 'desc'),
+          limit(3)
+        );
+        const accommodationsSnapshot = await getDocs(q);
+        const accommodationsData = accommodationsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setAccommodations(accommodationsData);
+      } catch (error) {
+        console.error('Error fetching accommodations:', error);
+      }
     };
+
+    fetchAccommodations();
+  }, []);
+
+  const handleCardClick = (accommodationId) => {
+    const accommodation = accommodations.find(
+      (item) => item.id === accommodationId
+    );
+    console.log("View clicked:", accommodation);
+    // Redirect to the page displaying the specific listing
+    window.location.href = `/display-listing/${accommodationId}`;
+  };
+
+  const handleExploreClick = () => {
+    window.location.href = "/admin-explore";
+  }
+
   return (
     <>
-    <div className='listing container'>
+      <div className="listing container">
         <div className="listing-h2">
-        <h2 className='option-h2'>See current listings</h2>
-    </div>
-
-    <div className='listing-row'>
-
-        <div className="listing-card">
-            <div className="card-img">
-                <img src="src\assets\images\peniel.jpg" alt="Peniel Boarding House" />
-            </div>
-            <div className="card-text">
-                <h4 className='accommodation-name'>Peniel</h4>
-                <h4 className='accommodation-price'>3,400 PHP</h4>
-            </div>
-            <div className="card-text">
-                <h5 className='accommodation-occupants'>2 persons per room</h5>
-                <h5 className='accommodation-amenities'>Laundry, Wifi</h5>
-            </div>
+          <h2 className="option-h2">See current listings</h2>
         </div>
 
-        <div className="listing-card">
-            <div className="card-img">
-                <img src="src\assets\images\silva.jpg" alt="Silva Dormitory" />
+        <div className="listing-row">
+          {accommodations.map((accommodation) => (
+            <div
+              key={accommodation.id}
+              className="listing-card"
+              onClick={() => handleCardClick(accommodation.id)}
+            >
+              <div className="card-img">
+                {accommodation.photos && accommodation.photos.length > 0 && (
+                  <img src={accommodation.photos[0]} alt={accommodation.accName} />
+                )}
+              </div>
+              <div className="card-text">
+                <h4 className="accommodation-name">{accommodation.accName}</h4>
+                <h4 className="accommodation-price">PHP {accommodation.roomFee}</h4>
+              </div>
+              <div className="card-text">
+                <h5 className="accommodation-occupants">
+                  {accommodation.occupants} persons per room
+                </h5>
+                <h5 className="accommodation-amenities">
+                  {accommodation.amenities.join(', ')}
+                </h5>
+              </div>
             </div>
-            <div className="card-text">
-                <h4 className='accommodation-name'>Silva</h4>
-                <h4 className='accommodation-price'>5,000 PHP</h4>
-            </div>
-            <div className="card-text">
-                <h5 className='accommodation-occupants'>2 persons per room</h5>
-                <h5 className='accommodation-amenities'>Own CR, Wifi</h5>
-            </div>
+          ))}
         </div>
 
-        <div className="listing-card">
-            <div className="card-img">
-                <img src="src\assets\images\upad.jpg" alt="UPAD Boarding House" />
-            </div>
-            <div className="card-text">
-                <h4 className='accommodation-name'>UPAD</h4>
-                <h4 className='accommodation-price'>5,000 PHP</h4>
-            </div>
-            <div className="card-text">
-                <h5 className='accommodation-occupants'>2 persons per room</h5>
-                <h5 className='accommodation-amenities'>Own CR, Wifi</h5>
-            </div>
+        <div className="explore-btn">
+          <button onClick={handleExploreClick}>Explore</button>
         </div>
-
-    </div>
-
-    <div className='explore-btn'>
-        <button onClick={handleExploreClick}>
-            Explore
-        </button>
-    </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Listings
+export default Listings;
--- src/components/listings/Listings.jsx
+++ src/components/listings/Listings.jsx
@@ -1,70 +1,88 @@
-import React from 'react';
-import './listings.scss';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, doc, updateDoc, query, where, orderBy, limit } from 'firebase/firestore';
+import { db, auth } from '../../firebase';
+import "./listings.scss";
 
-export default function Option() {
-    const handleExploreClick = () => {
-        window.location.href = '/explore';
+const Listings = () => {
+  const [accommodations, setAccommodations] = useState([]);
+
+  useEffect(() => {
+    const fetchAccommodations = async () => {
+      try {
+        const accommodationsCollectionRef = collection(db, 'accommodations');
+        const q = query(
+          accommodationsCollectionRef,
+          orderBy('editedAt', 'desc'),
+          limit(3)
+        );
+        const accommodationsSnapshot = await getDocs(q);
+        const accommodationsData = accommodationsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setAccommodations(accommodationsData);
+      } catch (error) {
+        console.error('Error fetching accommodations:', error);
+      }
     };
 
-    return (
-        <div className='listing container'>
+    fetchAccommodations();
+  }, []);
 
-            <div className="listing-h2">
-                <h2 className='option-h2'>See current listings</h2>
-            </div>
+  const handleCardClick = (accommodationId) => {
+    const accommodation = accommodations.find(
+      (item) => item.id === accommodationId
+    );
+    console.log("View clicked:", accommodation);
+    // Redirect to the page displaying the specific listing
+    window.location.href = `/display-listing/${accommodationId}`;
+  };
 
-            <div className='listing-row'>
+  const handleExploreClick = () => {
+    window.location.href = "/explore";
+  }
 
-                <div className="listing-card">
-                    <div className="card-img">
-                        <img src="src\assets\images\peniel.jpg" alt="Peniel Boarding House" />
-                    </div>
-                    <div className="card-text">
-                        <h4 className='accommodation-name'>Peniel</h4>
-                        <h4 className='accommodation-price'>3,400 PHP</h4>
-                    </div>
-                    <div className="card-text">
-                        <h5 className='accommodation-occupants'>2 persons per room</h5>
-                        <h5 className='accommodation-amenities'>Laundry, Wifi</h5>
-                    </div>
-                </div>
-
-                <div className="listing-card">
-                    <div className="card-img">
-                        <img src="src\assets\images\silva.jpg" alt="Silva Dormitory" />
-                    </div>
-                    <div className="card-text">
-                        <h4 className='accommodation-name'>Silva</h4>
-                        <h4 className='accommodation-price'>5,000 PHP</h4>
-                    </div>
-                    <div className="card-text">
-                        <h5 className='accommodation-occupants'>2 persons per room</h5>
-                        <h5 className='accommodation-amenities'>Own CR, Wifi</h5>
-                    </div>
-                </div>
-
-                <div className="listing-card">
-                    <div className="card-img">
-                        <img src="src\assets\images\upad.jpg" alt="UPAD Boarding House" />
-                    </div>
-                    <div className="card-text">
-                        <h4 className='accommodation-name'>UPAD</h4>
-                        <h4 className='accommodation-price'>5,000 PHP</h4>
-                    </div>
-                    <div className="card-text">
-                        <h5 className='accommodation-occupants'>2 persons per room</h5>
-                        <h5 className='accommodation-amenities'>Own CR, Wifi</h5>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className='explore-btn'>
-                <button onClick={handleExploreClick}>
-                    Explore
-                </button>
-            </div>
-
+  return (
+    <>
+      <div className="listing container">
+        <div className="listing-h2">
+          <h2 className="option-h2">See current listings</h2>
         </div>
-    )
-}
+
+        <div className="listing-row">
+          {accommodations.map((accommodation) => (
+            <div
+              key={accommodation.id}
+              className="listing-card"
+              onClick={() => handleCardClick(accommodation.id)}
+            >
+              <div className="card-img">
+                {accommodation.photos && accommodation.photos.length > 0 && (
+                  <img src={accommodation.photos[0]} alt={accommodation.accName} />
+                )}
+              </div>
+              <div className="card-text">
+                <h4 className="accommodation-name">{accommodation.accName}</h4>
+                <h4 className="accommodation-price">PHP {accommodation.roomFee}</h4>
+              </div>
+              <div className="card-text">
+                <h5 className="accommodation-occupants">
+                  {accommodation.occupants} persons per room
+                </h5>
+                <h5 className="accommodation-amenities">
+                  {accommodation.amenities.join(', ')}
+                </h5>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="explore-btn">
+          <button onClick={handleExploreClick}>Explore</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Listings;
