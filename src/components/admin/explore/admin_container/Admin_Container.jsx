54186e21849e4c43843e7d876a8465dad84123f7
Sanyiah <slpiang@up.edu.ph>
2023-06-04 02:23:44 +0800

    updated Explore admin pages

--- src/components/admin/explore/admin_container/Admin_Container.jsx
+++ src/components/admin/explore/admin_container/Admin_Container.jsx
@@ -5,28 +5,45 @@ import Admin_Checklist from '../admin_checklist/Admin_Checklist';
 import Admin_Listing from '../admin_listing/Admin_Listing';
 
 const Admin_Container = ({ isLoaded, type }) => {
-    const [containerClass, setContainerClass] = useState('main-container');
-      
-    const updateContainerClass = (isDropdownVisible) => {
+  const [containerClass, setContainerClass] = useState('main-container');
+
+  const updateContainerClass = (isDropdownVisible) => {
     const newContainerClass =
-        isDropdownVisible && window.innerWidth <= 1000
-            ? 'main-container filtered'
-            : 'main-container';
-        setContainerClass(newContainerClass);
-    };
-      
-    return (
-        <>
-            <div className="explore-page container">
-              <div className={containerClass}>
-                <Admin_Checklist updateContainerClass={updateContainerClass} />
-                <div className="headline-listing-section">
-                  <Admin_Listing isLoaded={isLoaded} type={type}/>
-                </div>
-              </div>
-            </div>
-          </>
-        );
-    };
+      isDropdownVisible && window.innerWidth <= 1000
+        ? 'main-container filtered'
+        : 'main-container';
+    setContainerClass(newContainerClass);
+  };
+
+  const [filterValues, setFilterValues] = useState({
+    checkboxes: {},
+    numberInputs: {},
+    selectedOption: 'Select a type',
+  });
+
+  const updateFilters = (newFilters) => {
+    setFilterValues(newFilters);
+  };
+
+  return (
+    <>
+      <div className="explore-page container">
+        <div className={containerClass}>
+          <Admin_Checklist 
+            updateContainerClass={updateContainerClass}
+            updateFilters={updateFilters} 
+          />
+          <div className="headline-listing-section">
+            <Admin_Listing 
+              isLoaded={isLoaded} 
+              type={type}
+              filterValues={filterValues}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Admin_Container;
