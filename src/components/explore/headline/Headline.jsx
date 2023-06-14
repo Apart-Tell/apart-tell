e2f0b2b622a7a410b696d788e82ab66bfbd17c5b
DanicaVont <daapostol2@up.edu.ph>
2023-06-15 04:25:35 +0800

    added "loading..." as spinner

--- src/components/explore/headline/Headline.jsx
+++ src/components/explore/headline/Headline.jsx
@@ -2,26 +2,40 @@ import React from 'react';
 import './headline.scss';
 
 const Headline = ({ isLoaded, type, accommodationsCount }) => {
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        setIsLoading(true);
+
+        // Simulating API fetch
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
 
     let headlineText = '';
 
-    if (isLoaded) {
+    if (isLoading) {
+        headlineText = 'Loading...'; // Display loading text
+    } else if (isLoaded) {
         switch (type) {
-        case "Apartment":
-            headlineText = "Apartment";
-            break;
-        case "Boarding house":
-            headlineText = "Boarding House";
-            break;
-        case "Dormitory":
-            headlineText = 'Dormitory';
-            break;
-        default:
-            headlineText = 'Where to stay in Mintal?';
-            break;
+            case 'Apartment':
+                headlineText = 'Apartment';
+                break;
+            case 'Boarding house':
+                headlineText = 'Boarding House';
+                break;
+            case 'Dormitory':
+                headlineText = 'Dormitory';
+                break;
+            default:
+                headlineText = 'Where to stay in Mintal?';
+                break;
         }
     } else if (accommodationsCount > 0) {
-        headlineText = `${accommodationsCount} accommodation${accommodationsCount > 1 ? 's': ''} found!`;
+        headlineText = `${accommodationsCount} accommodation${
+            accommodationsCount > 1 ? 's' : ''
+        } found!`;
     } else {
         headlineText = 'No accommodations found!';
     }
@@ -29,13 +43,16 @@ const Headline = ({ isLoaded, type, accommodationsCount }) => {
     return (
         <>
             <div className="headline-section">
+                {isLoading ? (
+                    <div className="spinner">loading...</div>
+                ) : (
                     <div className="headline-text">
                         <h2>{headlineText}</h2>
                     </div>
+                )}
             </div>
         </>
-    )
-
-}
+    );
+};
 
 export default Headline;
